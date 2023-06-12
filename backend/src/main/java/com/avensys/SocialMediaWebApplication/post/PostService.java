b9b6e1806ba44a9b965a064a419b60a0556c9251
He Xiang Koh <kohhxx6@gmail.com>
2023-06-12 23:26:24 +0800

    Added file util class to handle file extension and test create and delete of post with images and video

--- backend/src/main/java/com/avensys/SocialMediaWebApplication/post/PostService.java
+++ backend/src/main/java/com/avensys/SocialMediaWebApplication/post/PostService.java
@@ -1,10 +1,17 @@
 package com.avensys.SocialMediaWebApplication.post;
 
+import com.avensys.SocialMediaWebApplication.cloudinary.CloudinaryHelper;
 import com.avensys.SocialMediaWebApplication.exceptions.ResourceNotFoundException;
+import com.avensys.SocialMediaWebApplication.user.User;
 import com.avensys.SocialMediaWebApplication.user.UserRepository;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -12,44 +19,64 @@ public class PostService {
 
     private final PostRepository postRepository;
     private final UserRepository userRepository;
+    private final CloudinaryHelper cloudinaryHelper;
 
-    public PostService(PostRepository postRepository, UserRepository userRepository) {
+    public PostService(PostRepository postRepository, UserRepository userRepository, CloudinaryHelper cloudinaryHelper) {
         this.postRepository = postRepository;
         this.userRepository = userRepository;
+        this.cloudinaryHelper = cloudinaryHelper;
     }
 
-    public List<Post> findAllPosts(){
+    public List<Post> findAllPosts() {
         return postRepository.findAll();
     }
 
-    public Post findPostById(long id){
+    public Post findPostById(long id) {
         Optional<Post> post = postRepository.findById(id);
-        if (post.isPresent()){
+        if (post.isPresent()) {
             return post.get();
         } else {
             throw new ResourceNotFoundException("Post with id %s not found".formatted(id));
         }
     }
 
-//    public Post createPost(PostCreateRequestDTO postCreateRequest, Principal principal) {
-//        Optional<User> user = userRepository.findByEmail(principal.getName());
-//        if (!user.isPresent()) {
-//            throw new UsernameNotFoundException("User not found");
-//        }
-//        Post post = new Post();
-//        post.setTitle(postCreateRequest.title());
-//        post.setCaption(postCreateRequest.caption());
-//
-//        if (postCreateRequest.link() != null){
-//            post.setContentUrl(postCreateRequest.link());
-//        } else if (postCreateRequest.file() != null){
-//
-//        }
-//
-//    }
+    public PostCreateResponseDTO createPost(PostCreateRequestDTO postCreateRequest) {
+        Principal principal = SecurityContextHolder.getContext().getAuthentication();
+        Optional<User> user = userRepository.findByEmail(principal.getName());
+        if (!user.isPresent()) {
+            throw new UsernameNotFoundException("User not found");
+        }
+        Post post = new Post();
+        post.setTitle(postCreateRequest.title());
+        post.setCaption(postCreateRequest.caption());
+
+        if (postCreateRequest.link() != null && !postCreateRequest.link().isEmpty()) {
+            post.setContentUrl(postCreateRequest.link());
+        } else if (postCreateRequest.file() != null && !postCreateRequest.file().isEmpty()) {
+            try {
+                Map uploadResult = cloudinaryHelper.upload(postCreateRequest.file());
+                post.setContentUrl((uploadResult.get("url").toString()));
+                post.setContentId((uploadResult.get("public_id").toString()));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        post.setUser(user.get());
+        Post postSaved = postRepository.save(post);
+        PostCreateResponseDTO postCreateResponse = new PostCreateResponseDTO(postSaved.getId(), postSaved.getTitle(), postSaved.getCaption(), postSaved.getContentUrl(), postSaved.getCreatedAt(), postSaved.getUpdatedAt());
+        return postCreateResponse;
+    }
 
     public void deletePostById(long id) {
         Post post = findPostById(id);
+        if (post.getContentId() != null && !post.getContentId().isEmpty()) {
+            try {
+                Map deleteResult = cloudinaryHelper.delete(post.getContentId(),post.getContentUrl());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         postRepository.delete(post);
     }
 
