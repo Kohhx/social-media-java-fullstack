674d6eb8a804b2a0995f80a68cc8d40659038ae9
tan29121996 <tan29121996@gmail.com>
2023-06-12 21:58:07 +0800

    Used ngFor to generate multiple posts

--- frontend/src/app/component/feed/feed.component.ts
+++ frontend/src/app/component/feed/feed.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./feed.component.css']
 })
 export class FeedComponent {
-
+  items = [
+    { name: 'John Doe', timestamp: '2023-06-12', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.', imageUrl: 'https://img.freepik.com/premium-vector/business-global-economy_24877-41082.jpg' },
+    { name: 'Jane Smith', timestamp: '2023-06-11', content: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', imageUrl: 'https://png.pngtree.com/png-vector/20190704/ourmid/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg' },
+  ];
 }
