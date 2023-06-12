6bc274524de9f33bd1620844e5593b04da90a75d
tan29121996 <tan29121996@gmail.com>
2023-06-12 21:43:43 +0800

    Updated routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
+import { FeedComponent } from './component/feed/feed.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'feed', component: FeedComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FeedComponent } from './feed/feed.component';
+import { FeedComponent } from './component/feed/feed.component';
 import { LoginComponent } from './component/login/login.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { RegisterComponent } from './component/register/register.component';
