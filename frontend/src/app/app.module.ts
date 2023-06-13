da4670028a6d8d6393f5516669ecf90be224b2c0
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-13 09:09:26 +0800

    remove repeated imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,8 +12,6 @@ import { FeedComponent } from './feed/feed.component';
 import { LoginComponent } from './component/login/login.component';
 import { NavbarComponent } from './component/navbar/navbar.component';
 import { RegisterComponent } from './component/register/register.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
