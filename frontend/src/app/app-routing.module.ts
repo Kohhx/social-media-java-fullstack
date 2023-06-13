ead1fdfcd742ce6e70c311cf0b48fd66af2af4a9
He Xiang Koh <kohhxx6@gmail.com>
2023-06-13 16:04:02 +0800

    Added login funtionalty and routeguards and router children

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './component/login/login.component';
 import { FeedComponent } from './component/feed/feed.component';
 import { RegisterComponent } from './component/register/register.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { userguardGuard } from './guards/userguard.guard';
+import { adminguardGuard } from './guards/adminguard.guard';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'feed', component: FeedComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', component: NavbarComponent, children: [
+    { path: 'feed', component: FeedComponent, canActivate: [userguardGuard] },
+  ] },
   { path: '**', redirectTo: '/login', pathMatch: 'full' },
 ];
 
--- frontend/src/app/component/login/login.component.ts
+++ frontend/src/app/component/login/login.component.ts
@@ -1,14 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/service/authentication/authentication.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginFormGroup!: FormGroup;
 
-  onSubmit() {
-    console.log("Handling the login button.")
+  constructor(
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService,
+    private router: Router,
+    private toastr: ToastrService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginFormGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+  onSubmit() {
+    console.log('Handling the login button.');
+  }
+
+  get email() {
+    return this.loginFormGroup.get('email');
+  }
+  get password() {
+    return this.loginFormGroup.get('password');
+  }
+
+  login() {
+    const email = this.email?.value;
+    const password = this.password?.value;
+
+    this.authenticationService.login(email, password).subscribe({
+      next: (data) => {
+        console.log('Login successful');
+        this.toastr.success('Login successful');
+        this.router.navigate(['feed'])
+      }
+    });
+  }
 }
