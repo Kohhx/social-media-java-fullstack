1e5e8fe7d1f4505303aaebc4baf4b87ae8187fae
jcmonkey87 <fuzhonglong@gmail.com>
2023-06-13 19:45:37 +0800

    add post service

--- frontend/src/app/service/post/post.service.ts
+++ frontend/src/app/service/post/post.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 interface Post {
   id: number;
@@ -15,9 +17,20 @@ interface Post {
 export class PostService {
 
   private BASE_URL: string = 'http://localhost:8080/api';
-  private TOKEN_PREFIX: string = 'Bearer ';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
+  getAllPosts(): Observable<Post[]> {
+    // To retreive the token from the session storage, we need to set the header
+    const headers = new HttpHeaders().set('Authorization', 'Bearer ' + this.getAuthenticationToken());
+
+    // Return the response from the server
+    return this.http.get<Post[]>(`${this.BASE_URL}/posts`, { headers });
+  }
+
+  // Getting the token from the session storage
+  private getAuthenticationToken(): string {
+    return sessionStorage.getItem('token');
+  }
 
 }
