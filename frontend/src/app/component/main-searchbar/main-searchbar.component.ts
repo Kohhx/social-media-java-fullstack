c10f7522aed99fa4b5ed54b4ac3044fab1d8bc88
He Xiang Koh <kohhxx6@gmail.com>
2023-06-17 22:26:56 +0800

    Fixed some bug with search bar

--- frontend/src/app/component/main-searchbar/main-searchbar.component.ts
+++ frontend/src/app/component/main-searchbar/main-searchbar.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/service/user/user.service';
 
 @Component({
   selector: 'app-main-searchbar',
   templateUrl: './main-searchbar.component.html',
-  styleUrls: ['./main-searchbar.component.css']
+  styleUrls: ['./main-searchbar.component.css'],
 })
 export class MainSearchbarComponent implements OnInit {
   searchBarForm!: FormGroup;
@@ -13,40 +14,43 @@ export class MainSearchbarComponent implements OnInit {
   defaultProfileImage =
     'https://w7.pngwing.com/pngs/754/2/png-transparent-samsung-galaxy-a8-a8-user-login-telephone-avatar-pawn-blue-angle-sphere-thumbnail.png';
 
-  constructor(private fb:FormBuilder, private userService: UserService) { }
+  constructor(private router:Router ,private fb: FormBuilder, private userService: UserService) {}
 
   ngOnInit(): void {
     this.searchBarForm = this.fb.group({
-      search: ['']
-    })
+      search: [''],
+    });
 
     this.searchBarForm.valueChanges.subscribe({
       next: (value) => {
-        if (value.search == '') {
+        if (value.search == null) {
           this.searchResults = [];
-          return;
+        } else {
+          this.userService.findUsersByParams(value.search).subscribe({
+            next: (data) => {
+              console.log(data);
+              this.searchResults = data;
+            },
+          });
         }
-        this.userService.findUsersByParams(value.search).subscribe({
-          next: (data) => {
-            console.log(data)
-            this.searchResults=data
-          }
-        })
       },
       error: (err) => {
-        console.log(err)
-      }
-    })
+        console.log(err);
+      },
+    });
   }
 
-  get search () { return this.searchBarForm.get('search')!;}
-
-
-  getUserLink(id: number){
-    return `/users/${id}/posts`
+  get search() {
+    return this.searchBarForm.get('search')!;
   }
 
-  resetField() {
+  getUserLink(id: number) {
+    return `/users/${id}/posts`;
+  }
+
+  goToProfile(id: number) {
     this.searchBarForm.reset();
+    this.searchResults = [];
+    this.router.navigate([`/users/${id}/posts`]);
   }
 }
